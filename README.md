97f9fca3f6dbb00e771bd7e30378ca7c513d6f12
Mike O'Toole <mike@otoole.io>
2015-10-01 12:41:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#Leaflet.DoubleRightClickZoom
+======================
+
+An interaction handler that allows the map to be zoomed out by double
+right click.
+
+##Usage
+
+Double right click zoom is enabled automatically simply through including
+the plugin or can be controlled explicitly with the `doubleRightClickZoom` map option.
+
+````javascript
+var map = L.map('map', {
+    doubleRightClickZoom: true
+ });
+````
+
+##License
+This software is released under the [MIT licence](http://opensource.org/licenses/MIT).
---
+++ src/leaflet.rightclickzoom.js
@@ -0,0 +1,36 @@
+// Leaflet.DoubleRightClickZoom v1.0
+// https://github.com/GhostGroup/Leaflet.DoubleRightClickZoom
+// Lets users double right click to zoom out on all maps.
+
+L.Map.mergeOptions({
+  doubleRightClickZoom: true
+});
+
+L.Map.DoubleRightClickZoom = L.Handler.extend({
+  addHooks: function() {
+    this._map.clicks = 0;
+    this._map.on('contextmenu', this._onDoubleClick, this);
+  },
+  removeHooks: function() {
+    this._map.off('contextmenu', this._onDoubleClick, this);
+  },
+  _onDoubleClick: function(e) {
+    this._map.clicks++;
+    if (this._map.clicks === 1) {
+      setTimeout(((function(_this) {
+        return function() {
+          if (_this._map.clicks === 1) {
+            console.log('single click!');
+          } else {
+            console.log('double click!');
+            _this._map.setZoom(Math.ceil(_this._map.getZoom()) - 1);
+          }
+          _this._map.clicks = 0;
+        };
+      })(this)), 300);
+    }
+    return false;
+  }
+});
+
+L.Map.addInitHook('addHandler', 'doubleRightClickZoom', L.Map.DoubleRightClickZoom);
